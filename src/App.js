9b7fb771071cfa8c33a5c5cee9176ac23f9daa58
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-01-07 21:11:38 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
+import CharacterList from './components/CharacterList';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people')
+      .then(res => res.json())
+      .then(result => {
+        setCharacters(result.results);
+      })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CharacterList characters={characters} />
       </header>
     </div>
   );
